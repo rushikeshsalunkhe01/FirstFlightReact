080726f49add04c7e84f19b276fd5eefaccc78e4
rushikeshsalunkhe01 <rushisalunkhe111053@gmail.com>
2025-01-10 16:00:44 +0530

    add all foldrs and fils

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+
+// Find the root element
+const rootElement = document.getElementById("root");
+
+// Create a React root
+const root = createRoot(rootElement);
+
+// Render content
+root.render(<h1>First Flight... Bhurrrrrr🚀❤</h1>);
